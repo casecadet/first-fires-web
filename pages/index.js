3827a72fcf4475f38ef526c280b57f316a575b0b
Casey McGraw-Herdeg <37131033+casecadet@users.noreply.github.com>
2022-01-23 14:58:46 -0500

    Meta info

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,9 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>First Fires has a website?</title>
+        <link rel="icon" href="/favifire.ico" />
+        {/* Favicon Copyright 2020 Twitter, Inc and other contributors: https://twemoji.twitter.com/ */}
       </Head>
 
       <main>
